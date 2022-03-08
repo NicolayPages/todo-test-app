236f124e6c9882d29d0a95ba04c1ca9bc310ddac
NicolayPages <nicolaj02@mail.ru>
2022-03-08 22:17:58 +0500

    master

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from "react-router-dom";
 import { publicRoutes } from '../../routes/routes';
+import Preloader from '../Preloader/Preloader';
 
 const AppRouter: React.FC = React.memo(() => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Preloader />}>
             <Routes>
                 {publicRoutes.map(route =>
                     <Route
